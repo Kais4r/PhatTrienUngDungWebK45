b2124d1100a916b76fedd68eb50cf0f03df912b5
Kais4r <143178887+Kais4r@users.noreply.github.com>
2023-11-01 07:51:04 +0700

    Update bài tập

---
+++ Bài học/Bài 5 Web back-end - Lập trình phía server/1. Bài tập/0.Thực hành/Bài 2/bai2.2.php
@@ -0,0 +1,7 @@
+<?php
+	echo "Tomorrow I 'll learn PHP global variables."."\n";
+	echo "This is a bad command : del c:\\*.*"."\n";
+	echo "This is second way to write: \n";
+	echo 'Tomorrow I \'ll learn PHP global variables.'."\n";
+	echo 'This is a bad command : del c:\\*.* '."\n";
+?>
